55ef455cb73f00299600ea9fec52599417b3bbd3
Akshay Ratnakar <ratnakarakshay902@gmail.com>
2023-12-11 17:36:14 +0530

    user controller steps

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,23 @@
 import {asyncHandler } from '../utils/asyncHandler.js'
 
 const registerUser=asyncHandler( async (req, res)=>{
-    res.status(200).json({
-        message:"OK"
-    })
+    //get user details from request
+
+    //validation
+
+    //check if user already exists
+
+    //check for images, check for avatar
+
+    //upload them to cloudinary
+
+    //create user object - create entry in DB
+
+    //remove unnecessary data for response
+
+    //check for user creation
+
+    //return response
 })
 
 export default registerUser
