c912ac4357bced762c4f392c15ac70452fcaf5cc
Akshay Ratnakar <ratnakarakshay902@gmail.com>
2023-12-19 20:21:04 +0530

    bullmq implementation

--- src/jobs/user.register.job.ts
+++ src/jobs/user.register.job.ts
@@ -1,52 +1,80 @@
 import axios from 'axios';
-import cron from 'node-cron';
+import { Queue, Worker, QueueScheduler } from 'bullmq';
 import { User } from '../models/user.model';
-const fetchDataAndStore = async () => {
-    try {
-      // Fetch data from the Random User Generator API
-      const response = await axios.get(process.env.API_URL!);
-      const randomUserData = response.data.results;
-  
-      // Map fetched data to match the User schema
-      const userData = randomUserData.map((randomUser: any) => ({
-        id: randomUser.login.uuid,
-        gender: randomUser.gender,
-        name: `${randomUser.name.title} ${randomUser.name.first} ${randomUser.name.last}`,
-        address: {
-          city: randomUser.location.city,
-          state: randomUser.location.state,
-          country: randomUser.location.country,
-          street: `${randomUser.location.street.number} ${randomUser.location.street.name}`,
-        },
-        email: randomUser.email,
-        age: randomUser.dob.age.toString(),
-        picture: randomUser.picture.large,
-      }));
-  
-      // Store data in MongoDB
-      await User.insertMany(userData);
-  
-      console.log('Data stored successfully!');
-    } catch (error: any) {
-      console.error('Error fetching and storing data:', error.message);
-      throw error;
-    }
-  };
-  
+import { config } from 'dotenv';
 
-const startDataFetchingJob = () => {
+config({
+  path: './.env',
+});
+
+const BATCH_SIZE = parseInt(process.env.BATCH_SIZE!, 10); // Set your desired batch size
+
+const fetchDataAndStoreBatch = async (batchId: number, data: any[]) => {
   try {
-    // Run the data fetching job immediately
-    fetchDataAndStore();
+    const startIdx = (batchId - 1) * BATCH_SIZE;
+    const endIdx = startIdx + BATCH_SIZE;
+    const batchData = data.slice(startIdx, endIdx);
 
-    // Schedule the data fetching job using node-cron to run every 2 hours
-    const jobSchedule = '0 */2 * * *'; // Run every 2 hours, adjust as needed
-    cron.schedule(jobSchedule, fetchDataAndStore);
+    // Map fetched data to match the User schema
+    const userData = batchData.map((randomUser) => ({
+      id: randomUser.login.uuid,
+      gender: randomUser.gender,
+      name: `${randomUser.name.title} ${randomUser.name.first} ${randomUser.name.last}`,
+      address: {
+        city: randomUser.location.city,
+        state: randomUser.location.state,
+        country: randomUser.location.country,
+        street: `${randomUser.location.street.number} ${randomUser.location.street.name}`,
+      },
+      email: randomUser.email,
+      age: randomUser.dob.age.toString(),
+      picture: randomUser.picture.large,
+    }));
 
-    console.log('Data fetching job scheduled!');
+    // Store data in MongoDB
+    await User.insertMany(userData);
+
   } catch (error:any) {
-    console.error('Error scheduling data fetching job:', error.message);
+    throw error;
   }
 };
 
-export { fetchDataAndStore, startDataFetchingJob };
+const fetchAndStoreDataRecursively = async (startBatch: number) => {
+  try {
+    // Fetch data from the Random User Generator API
+    const response = await axios.get(process.env.API_URL!);
+
+    const randomUserData = response.data.results;
+
+    // Calculate the number of batches
+    const totalBatches = Math.ceil(1000 / BATCH_SIZE);
+
+    // Enqueue the data fetching and storing jobs for each batch
+    for (let i = startBatch; i <= totalBatches; i++) {
+      await queue.add('fetchDataJob', { batchId: i, data: randomUserData });
+    }
+  } catch (error:any) {
+    throw error;
+  }
+};
+
+const queueName = 'fetchDataQueue';
+const queue = new Queue(queueName);
+const scheduler = new QueueScheduler(queueName);
+
+const worker = new Worker(queueName, async (job) => {
+  const batchId = job.data.batchId;
+  const data = job.data.data;
+  await fetchDataAndStoreBatch(batchId, data);
+});
+
+const startDataFetchingJob = async () => {
+  try {
+    // Start the recursive data fetching and storing process from batch 1
+    await fetchAndStoreDataRecursively(1);
+
+  } catch (error:any) {
+  }
+};
+
+export { startDataFetchingJob };
